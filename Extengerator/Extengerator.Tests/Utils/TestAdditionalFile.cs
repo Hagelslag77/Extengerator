a485a689eb6c1684ec3373e02ef70f7c4d9c4151
Hagelslag77 <andreas.kleer@gmail.com>
2024-01-16 21:54:25 +0100

    changed: TestAdditionalFile allows null as (source) text

--- Extengerator/Extengerator.Tests/Utils/TestAdditionalFile.cs
+++ Extengerator/Extengerator.Tests/Utils/TestAdditionalFile.cs
@@ -4,17 +4,11 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace Extengerator.Tests.Utils;
 
-public class TestAdditionalFile : AdditionalText
+public class TestAdditionalFile(string path, string? text) : AdditionalText
 {
-    private readonly SourceText _text;
+    private readonly SourceText? _text = text is null ? null : SourceText.From(text);
+    
+    public override string Path { get; } = path;
 
-    public TestAdditionalFile(string path, string text)
-    {
-        Path = path;
-        _text = SourceText.From(text);
-    }
-
-    public override SourceText GetText(CancellationToken cancellationToken = new()) => _text;
-
-    public override string Path { get; }
+    public override SourceText? GetText(CancellationToken cancellationToken = new()) => _text;
 }
